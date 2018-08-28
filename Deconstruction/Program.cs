27ddec29b77ceb001e891b58ece1e27fd75af2d5
jok405 <john.y.kim84@gmail.com>
2018-08-28 15:55:34 -0400

    initial commit

---
+++ Deconstruction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Deconstruction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner runner = new Runner();
+            runner.Run();
+        }
+    }
+    public class Runner
+    {
+        public void Run()
+        {
+            (int hour, int minutes, int seconds) = GetTime();
+
+            if (minutes > 30)
+            {
+                hour++;
+            }
+
+            WriteLine($"{hour}:{minutes}:{seconds}");
+        }
+        public (int , int , int) GetTime()
+        {
+            return (1, 35, 40);
+        }
+        public void Run2()
+    }
+}
